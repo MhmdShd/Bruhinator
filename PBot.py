5c46084c206084079aeb91b1f63f68d2584e7790
MhmdShd <80423597+MhmdShd@users.noreply.github.com>
2021-08-02 16:58:03 +0300

    Update PBot.py
    indent fixed

--- PBot.py
+++ PBot.py
@@ -155,27 +155,27 @@ async def _playcommand(ctx, *, search: str):
     voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
     if 'https://' in search:
         url = search
-    else:
-        await ctx.send(f'`searching for {search}`')
-    query_string = urllib.parse.urlencode({'search_query': search})
-        htm_content = urllib.request.urlopen(
-            'http://www.youtube.com/results?' + query_string)
-        search_results = re.findall(r'/watch\?v=(.{11})', htm_content.read().decode())
-        url = 'http://www.youtube.com/watch?v=' + search_results[0]
-    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-    ydl_opts = {'format': 'best'}
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(str(url), download=False)
-        URL = info['formats'][0]['url']
-    FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn', }
-    player = discord.FFmpegPCMAudio(URL, **FFMPEG_OPTIONS)
-    if len(que) == 0:
-        voice.play(player, after=lambda x=None: play_next(voice))
-        await ctx.send('**Song is being played**')
-        que.append(player)
-    else:
-        await ctx.send(f'**Song queued** {thumbs_up}')
-        que.append(player)
+     else:
+            await ctx.send(f'`searching for {search}`')
+            query_string = urllib.parse.urlencode({'search_query': search})
+            htm_content = urllib.request.urlopen(
+                'http://www.youtube.com/results?' + query_string)
+            search_results = re.findall(r'/watch\?v=(.{11})', htm_content.read().decode())
+            url = 'http://www.youtube.com/watch?v=' + search_results[0]
+        voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+        ydl_opts = {'format': 'best'}
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(str(url), download=False)
+            URL = info['formats'][0]['url']
+        FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn', }
+        player = discord.FFmpegPCMAudio(URL, **FFMPEG_OPTIONS)
+        if len(que) == 0:
+            voice.play(player, after=lambda x=None: play_next(voice))
+            await ctx.send('**Song is being played**')
+            que.append(player)
+        else:
+            await ctx.send(f'**Song queued** {thumbs_up}')
+            que.append(player)
 
         
 def play_next(voice):
