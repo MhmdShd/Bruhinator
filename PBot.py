c202ab6b68d784f05fb02a50553f68bd134c6efe
MhmdShd <80423597+MhmdShd@users.noreply.github.com>
2021-09-18 20:10:25 +0300

    fixed play bug

--- PBot.py
+++ PBot.py
@@ -142,7 +142,6 @@ async def _join(ctx):
 async def _playcommand(ctx, *, search: str):
     global que
     global url
-    global repeat
     try:
         await ctx.author.voice.channel.connect()
     except:
@@ -151,29 +150,29 @@ async def _playcommand(ctx, *, search: str):
         else:
             await ctx.send(':x: **I am being controlled by another voice channel **:confused:')
             return
-        if 'https://' in search:
-            url = search
-        else:
-            await ctx.send(f'`searching for {search}`')
-            query_string = urllib.parse.urlencode({'search_query': search})
-            htm_content = urllib.request.urlopen(
-                'http://www.youtube.com/results?' + query_string)
-            search_results = re.findall(r'/watch\?v=(.{11})', htm_content.read().decode())
-            url = 'http://www.youtube.com/watch?v=' + search_results[0]
-        voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-        ydl_opts = {'format': 'best'}
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(str(url), download=False)
-            URL = info['formats'][0]['url']
-        FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn', }
-        player = discord.FFmpegPCMAudio(URL, **FFMPEG_OPTIONS)
-        if len(que) == 0:
-          voice.play(player, after=lambda x=None: play_next(voice))
-          await ctx.send('**Song is being played**')
-          que.append(player)
-        else:
-          await ctx.send(f'**Song queued** {thumbs_up}')
-          que.append(player)
+    if 'https://' in search:
+        url = search
+    else:
+        await ctx.send(f'`searching for {search}`')
+        query_string = urllib.parse.urlencode({'search_query': search})
+        htm_content = urllib.request.urlopen(
+            'http://www.youtube.com/results?' + query_string)
+        search_results = re.findall(r'/watch\?v=(.{11})', htm_content.read().decode())
+        url = 'http://www.youtube.com/watch?v=' + search_results[0]
+    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    ydl_opts = {'format': 'best'}
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(str(url), download=False)
+        URL = info['formats'][0]['url']
+    FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn', }
+    player = discord.FFmpegPCMAudio(URL, **FFMPEG_OPTIONS)
+    if len(que) == 0:
+      voice.play(player, after=lambda x=None: play_next(voice))
+      await ctx.send('**Song is being played**')
+      que.append(player)
+    else:
+      await ctx.send(f'**Song queued** {thumbs_up}')
+      que.append(player)
 
         
 def play_next(voice):
@@ -189,12 +188,6 @@ async def link(ctx):
     global url
     await ctx.send(f'song being played: {url}')
 
-
-@bot.command()
-async def repeat(ctx):
-    global repeat
-    repeat = True
-    await ctx.message.add_reaction(thumbs_up)
 
 
 
