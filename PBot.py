f5d7cc44cd45d8ec173d5c62a6bc092bb3f1c5ff
MhmdShd <80423597+MhmdShd@users.noreply.github.com>
2021-12-11 13:28:50 +0200

    .

--- PBot.py
+++ PBot.py
@@ -58,21 +58,8 @@ async def ping(ctx):
 # join voice
 @bot.command(aliases=['connect', 'CONNECT', 'Connect', 'join', 'JOIN', 'Join'])
 async def _joinCommand(ctx):
-    
     if ctx.author.voice:
-        await ctx.author.voice.channel.connect()
-    else:
-        await ctx.send(':x: **You should be in a voice channel to use this command**')
-
-
-
-# play music
-@bot.command(aliases=['play', 'p', 'Play', 'PLAY'])
-async def _playCommand(ctx, *, search: str):
-    global que
-    global url
-    if ctx.author.voice:
-        if not ctx.voice_client.channel:
+        if not ctx.voice_client:
             await ctx.author.voice.channel.connect()
 
         elif ctx.author.voice.channel != ctx.voice_client.channel:
@@ -80,38 +67,42 @@ async def _playCommand(ctx, *, search: str):
     else:
         await ctx.send(':x: **You should be in a voice channel to use this command**')
 
-    
-#     try:
-#         await ctx.author.voice.channel.connect()
-#     except:
-#         if ctx.author.voice.channel == ctx.voice_client.channel:
-#             print('already in voice channel')
-#         else:
-#             await ctx.send(':x: **I am being controlled by another voice channel **:confused:')
-#             return
-    if 'https://' in search:
-        url = search
+# play music
+@bot.command(aliases=['play', 'p', 'Play', 'PLAY'])
+async def _playCommand(ctx, *, search: str):
+    global que
+    global url
+
+    if ctx.author.voice:
+        if not ctx.voice_client:
+            await ctx.author.voice.channel.connect()
+            if 'https://' in search:
+                url = search
+            else:
+                query_string = urllib.parse.urlencode({'search_query': search})
+                htm_content = urllib.request.urlopen(
+                    'http://www.youtube.com/results?' + query_string)
+                search_results = re.findall(r'/watch\?v=(.{11})', htm_content.read().decode())
+                url = 'http://www.youtube.com/watch?v=' + search_results[0]
+            voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+            ydl_opts = {'format': 'best'}
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                info = ydl.extract_info(str(url), download=False)
+                URL = info['formats'][0]['url']
+            FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+                              'options': '-vn', }
+            player = discord.FFmpegPCMAudio(URL, **FFMPEG_OPTIONS)
+            if len(que) == 0:
+                voice.play(player, after=lambda x=None: play_next(voice))
+                await ctx.send('**Song is being played**')
+                que.append(player)
+            else:
+                await ctx.send(f'**Song queued** {thumbs_up}')
+                que.append(player)
+        elif ctx.author.voice.channel != ctx.voice_client.channel:
+            await ctx.send(':x: **I am being controlled by another voice channel **:confused:')
     else:
-        await ctx.send(f'`searching for {search}`')
-        query_string = urllib.parse.urlencode({'search_query': search})
-        htm_content = urllib.request.urlopen(
-            'http://www.youtube.com/results?' + query_string)
-        search_results = re.findall(r'/watch\?v=(.{11})', htm_content.read().decode())
-        url = 'http://www.youtube.com/watch?v=' + search_results[0]
-    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-    ydl_opts = {'format': 'best'}
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(str(url), download=False)
-        URL = info['formats'][0]['url']
-    FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn', }
-    player = discord.FFmpegPCMAudio(URL, **FFMPEG_OPTIONS)
-    if len(que) == 0:
-      voice.play(player, after=lambda x=None: play_next(voice))
-      await ctx.send('**Song is being played**')
-      que.append(player)
-    else:
-      await ctx.send(f'**Song queued** {thumbs_up}')
-      que.append(player)
+        await ctx.send(':x: **You should be in a voice channel to use this command**')
 
         
 def play_next(voice):
