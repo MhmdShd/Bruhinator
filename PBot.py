6b3376e45f942c85a7505fe9aef95888b8babd65
MhmdShd <mhmdalishd10@gmail.com>
2022-04-28 02:05:43 +0300

    refixed duration of song

--- PBot.py
+++ PBot.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-
+import json
 import discord
 import yt_dlp
 from discord.ext import commands
@@ -16,9 +16,9 @@ url = ''
 bot.remove_command('help')
 online_date = datetime
 def read_token():
-    with open('token.txt', 'r') as f:
-        lines = f.readlines()
-        return lines[0].strip()
+    config = open('config.json')
+    configs = json.load(config)
+    print(configs['token'])
 
 
 token = read_token()
